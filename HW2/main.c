9871fd4728d52244c05f34d49a19d5dd957481da
Tolga Kilinckaya <119072606+TolgaKilinckaya@users.noreply.github.com>
2023-04-27 01:21:34 +0300

    Update main.c

--- HW2/main.c
+++ HW2/main.c
@@ -14,9 +14,19 @@ void sort_polar();
 int main()
 {
     push(2,2);
+    push(-2,3);
     push(1,1);
     push(1,-1);
+    print();
+    sort();
+    print();
+
     sort_polar();
+    print();
     print_polar();
+    getch();
+    pop();
+    sort();
+    print();
     return 0;
 }
